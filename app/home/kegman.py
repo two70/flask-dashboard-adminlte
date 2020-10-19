edb2e94263eb953627fc48bfd1ba05cecb0936f4
two70 <jamw24@gmail.com>
2020-10-19 01:32:23 +0000

    setting up ajax calls for kegman

---
+++ app/home/kegman.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import sys
+import json
+from setproctitle import setproctitle
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), os.path.expanduser('~/raspilot')))
+from cereal import log, car
+from common.params import Params
+from common.profiler import Profiler
+from selfdrive.kegman_conf import kegman_conf
+
+setproctitle('webUI')
+
+frame_count = 0
+params = Params()
+profiler = Profiler(False, 'dashboard')
+user_id = str(params.get("PandaDongleId", True))
+user_id = user_id.replace("'","")
+
+try:
+  car_params = car.CarParams.from_bytes(params.get('CarParams', True))
+  kegman = kegman_conf(car_params)
+  kegman_valid = ('tuneRev' in kegman.conf)
+  print(kegman.conf)
+except:
+  print("kegman error")
+  kegman_valid = False
+  do_influx = False
+
